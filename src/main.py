648ae43b44c226edee7bf0f6f74f8eb5309c354b
SteffenHub <steffenhub@users.noreply.github.com>
2024-08-09 12:08:11 +0200

    results will be saved in a log file. if the calculation stops you can continue it later by passing the same parameters. If the same parameters entered the log file will be read and the previous choices, time and random generator will be set to the previous calculation.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import random
 import sys
 import time
 from copy import deepcopy
+from pathlib import Path
 from typing import Union
 
 from ortools.sat.python import cp_model
@@ -10,6 +11,16 @@ from ortools.sat.python.cp_model import CpSolverSolutionCallback
 
 from rule_builder import create_all_vars, add_all_rules_from_dimacs, create_freq_of_vars, get_sum_zero_freq, \
     get_sum_one_freq
+
+
+def save_choice(file_path: Path, used_random_code: str = None, var: str = None, frequency: int = None, time_till_now=None):
+    with file_path.open("a") as file:
+        if used_random_code is not None:
+            file.write(f"used random code : {used_random_code}\n")
+        if time_till_now is not None:
+            file.write(f"needed time : {time_till_now}\n")
+        if frequency is not None:
+            file.write(f"{var} : {frequency}\n")
 
 
 def get_value_of(var: cp_model.IntVar, model: cp_model.CpModel) -> Union[None, int]:
@@ -104,7 +115,8 @@ def ask_and_get_cnf_file() -> (str, int, list[list[int]]):
 
 def ask_and_get_seed() -> int:
     try:
-        seed = input("Insert seed for the random generator. example: 12345. Type None if a random seed should be used: ")
+        seed = input(
+            "Insert seed for the random generator. example: 12345. Type None if a random seed should be used: ")
         if seed != "None":
             seed = int(seed)
         else:
@@ -170,7 +182,8 @@ def main():
     create_freq_of_vars(number_of_variables=number_of_variables, model=model, all_vars=all_vars,
                         number_of_decimal_places=number_of_decimal_places)
 
-    zero_freq, one_freq = handle_should_have_zero_one_freq(model, number_of_variables, all_vars, number_of_decimal_places)
+    zero_freq, one_freq = handle_should_have_zero_one_freq(model, number_of_variables, all_vars,
+                                                           number_of_decimal_places)
 
     print("searching for a possible solution...")
     solver = cp_model.CpSolver()
@@ -187,6 +200,50 @@ def main():
     print("\nThis was one possible solution. Now we generate the frequencies with the random generator\n")
 
     var_list = list(range(1, number_of_variables + 1))
+    file_name_only = file_name.rsplit('/', 1)[-1].split('.')[0]
+    path_log_file = Path(
+        f'log_files/log_choice_file_{file_name_only}_{one_freq}_{zero_freq}_{number_of_decimal_places}_{seed}.txt')
+    path_log_file.parent.mkdir(parents=True, exist_ok=True)
+    if os.path.exists(os.fspath(path_log_file)):
+        print("Found log file with results!")
+        content = path_log_file.read_text().splitlines()
+
+        # check if last line is a used random code line
+        if content[len(content)-1].startswith("used random code"):
+            del content[len(content)-1]
+            with path_log_file.open('w') as file:
+                file.writelines('\n'.join(content))
+                file.write("\n")
+        content = path_log_file.read_text().splitlines()
+
+        time_previous = 0
+        used_random_times = 0
+        for line in content:
+            if (line.startswith("used cnf")
+                    or line.startswith("100%")
+                    or line.startswith("0%")
+                    or line.startswith("used decimal places")
+                    or line.startswith("used seed")):
+                continue
+            elif line.startswith("needed time"):
+                time_previous = float(line.split(" : ")[1].split(" ")[0])
+            elif line.startswith("used random code"):
+                used_random_times = eval(line.split(" : ")[1])
+            else:
+                choice = line.split(" : ")
+                model.Add(all_vars[choice[0] + "_freq"] == int(choice[1]))
+                var_list.remove(int(choice[0]))
+                print(f"Added frequency for var {choice[0]} = {choice[1]}")
+
+    else:
+        with path_log_file.open("a") as file:
+            file.write(f"used cnf : {file_name}\n")
+            file.write(f"100% vars : {one_freq}\n")
+            file.write(f"0% vars : {zero_freq}\n")
+            file.write(f"used decimal places : {number_of_decimal_places}\n")
+            file.write(f"used seed : {seed}\n")
+        time_previous = 0
+        used_random_times = 0
 
     print("Before we start to set random frequencies for the variables, you can set some vars manually")
     while True:
@@ -204,16 +261,19 @@ def main():
             continue
         if min_max[0] == min_max[1]:
             print("minimum and maximum for " + input_var + " are equal: " + str(min_max[0] / number_of_decimal_places))
+            save_choice(file_path=path_log_file, var=input_var, frequency=min_max[0], time_till_now=0, used_random_code=None)
             var_list.remove(int(input_var))
             continue
-        print("possible frequency for " + input_var + ": " + str(min_max[0] / number_of_decimal_places) + " - " + str(min_max[1] / number_of_decimal_places))
+        print("possible frequency for " + input_var + ": " + str(min_max[0] / number_of_decimal_places) + " - " + str(
+            min_max[1] / number_of_decimal_places))
         print("Which frequency should this variable have?")
         print("If you choose 10 for number of decimal places use 3 for 30% or 7 for 70%")
         print("If you choose 100 for number of decimal places use 30 for 30% or 73 for 73%")
         print("If you choose 1000 for number of decimal places use 300 for 30% or 732 for 73,2%")
         try:
             freq_for_input_var = input("Type in the frequency this variable should have: ")
-            correct_input = input(f"frequency for {input_var} will be set to {int(freq_for_input_var)/number_of_decimal_places}. Is this correct? [True, False]")
+            correct_input = input(
+                f"frequency for {input_var} will be set to {int(freq_for_input_var) / number_of_decimal_places}. Is this correct? [True, False]")
         except Exception as e:
             print("Unknown input! Try again")
             continue
@@ -221,21 +281,27 @@ def main():
             print("Try again")
             continue
         elif correct_input == "True":
-            print(f"I will set frequency for {input_var} to {int(freq_for_input_var)/number_of_decimal_places}")
+            print(f"I will set frequency for {input_var} to {int(freq_for_input_var) / number_of_decimal_places}")
         else:
             print("Unknown input! Use True or False. Try again")
             continue
         model.Add(all_vars[input_var + "_freq"] == int(freq_for_input_var))
+        save_choice(file_path=path_log_file, var=input_var, frequency=min_max[0], time_till_now=0, used_random_code=None)
         var_list.remove(int(input_var))
     print("start timer")
     start_time = time.time()
+    start_time = start_time - time_previous
+
     while var_list:
         var = random.choice(var_list)
+        used_random_times += 1
+        save_choice(file_path=path_log_file, used_random_code=f"random.choice({var_list})")
         var_list.remove(var)
         min_max = find_min_max_of_var(all_vars[str(var) + "_freq"], model)
         if min_max[0] == min_max[1]:
             print("minimum and maximum for " + str(var) + " are equal: " + str(min_max[0] / number_of_decimal_places))
             print(f"still missing {len(var_list)} variables")
+            save_choice(file_path=path_log_file, var=var, frequency=min_max[0], time_till_now=time.time() - start_time, used_random_code=None)
             continue
         print("possible frequency for " + str(var) + ": " + str(min_max[0] / number_of_decimal_places) + " - " + str(
             min_max[1] / number_of_decimal_places))
@@ -244,6 +310,7 @@ def main():
         tried_frequencies = []
         while not found_feasible_frequency:
             random_frequency = random.randint(min_max[0], min_max[1])
+            used_random_times += 1
             if random_frequency in tried_frequencies:
                 continue
             print("I've chosen: " + str(random_frequency))
@@ -257,6 +324,7 @@ def main():
                 tried_frequencies.append(random_frequency)
             else:
                 found_feasible_frequency = True
+                save_choice(file_path=path_log_file, var=var, frequency=random_frequency, time_till_now=time.time() - start_time, used_random_code=f"random.randint({min_max[0]}, {min_max[1]})")
                 model.Add(all_vars[str(var) + "_freq"] == random_frequency)
         print(f"still missing {len(var_list)} variables")
 
@@ -270,7 +338,8 @@ def main():
         lines.append(f"c used decimal places: {number_of_decimal_places}")
         lines.append(f"c used seed: {seed}")
         lines.append(f"c needed time: {time.time() - start_time} seconds")
-        [lines.append(f"{solver.Value(all_vars[str(var) + '_freq']) / number_of_decimal_places}") for var in range(1, number_of_variables + 1)]
+        [lines.append(f"{solver.Value(all_vars[str(var) + '_freq']) / number_of_decimal_places}") for var in
+         range(1, number_of_variables + 1)]
         filename = f"freq_result_{os.path.basename(file_name)}"
         open(filename, 'w').write('\n'.join(lines))
         print(f"Saved result as freq_result_{os.path.basename(file_name)}")
